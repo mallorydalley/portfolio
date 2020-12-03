6565f024590b1d22c0b803501ace5f1e7c2219c6
mallorydalley <malloryd28@gmail.com>
2020-12-02 18:57:57 -0700

    added db

---
+++ db/seed.sql
@@ -0,0 +1,34 @@
+create table graphics (
+    graphics_id serial primary key,
+    img text
+)
+
+create table luciano (
+    luciano_id serial primary key,
+    img text
+)
+
+create table taskbox (
+    taskbox_id serial primary key,
+    img text
+)
+
+create table flashdrills (
+    flashdrills_id serial primary key,
+    img text
+)
+
+create table cwc (
+    cwc_id serial primary key,
+    img text
+)
+
+create table formula (
+    formula_id serial primary key,
+    img text
+)
+
+create table emperitas (
+    emperitas_id serial primary key,
+    img text
+)
